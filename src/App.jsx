2dedd4db60f1340f8fbf0e97a71f56fcaa9e17ba
BilalMustafa123 <mbm15486@gmail.com>
2025-05-27 11:51:29 +0500

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import { Router, Route } from 'react-router-dom'
+import Signup from './Pages/Signup'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div>
-      <div className='text-3xl'> My name is Bilal </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Signup />} />
+        </Routes>
+
+      </Router>
     </div>
   )
 }
